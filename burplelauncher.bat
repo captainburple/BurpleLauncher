5ae594e3a4de0cab2dd4d1051ff3368ce1cafe1d
captainburple <87948924+captainburple@users.noreply.github.com>
2022-05-14 16:28:09 +0200

    Update burplelauncher.bat

--- burplelauncher.bat
+++ burplelauncher.bat
@@ -1,6 +1,5 @@
 @ECHO OFF
 ECHO Starting Minecraft,please wait...
-net start Minecraft
 net stop ClipSVC
 ECHO Done! Now open up Minecraft and enjoy!(dont close this window)
 Taskkill /Runtime Broker /f
